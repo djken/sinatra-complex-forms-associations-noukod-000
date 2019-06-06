f3606a8ce4ee2e549d32cf7c8681d8a60132d466
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-06-06 14:45:47 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -3,24 +3,24 @@ class PetsController < ApplicationController
   # Display what is in the index file from views/owners folders.
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
     erb :'/pets/new'
   end
 
-  post '/pets' do 
+  post '/pets' do
 
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
+  patch '/pets/:id' do
 
     redirect to "pets/#{@pet.id}"
   end
